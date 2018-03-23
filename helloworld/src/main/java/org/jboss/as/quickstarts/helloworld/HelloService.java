63c616d1ff4f646af371f659568272b52dc7189d
valerapeshchenko <35956219+valerapeshchenko@users.noreply.github.com>
2018-03-23 15:22:01 +0200

    Update HelloService.java

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -36,10 +36,15 @@ public class HelloService {
         Enumeration<URL> resources = getClass().getClassLoader().getResources("META-INF/MANIFEST.MF");
     while (resources.hasMoreElements()) {
     try {
-      Manifest manifest = new Manifest(resources.nextElement().openStream());
-      // check that this is your manifest and do what you need or get the next one
-          return_string += "Version: " + atts.getValue("Implementation-Version");
-          return_string += "Build: " + atts.getValue("Implementation-Build");
+      //Manifest manifest = new Manifest(resources.nextElement().openStream());
+      // check that this is your manifest and do what you need or get the next one        
+        Manifest mf = new Manifest();
+        mf.read(Thread.currentThread().getContextClassLoader().getResourceAsStream("META-INF/MANIFEST.MF"));
+
+        Attributes atts = mf.getMainAttributes();        
+        return_string += "Version: " + atts.getValue("Implementation-Version");
+        return_string += "Build: " + atts.getValue("Implementation-Build");
+        
     } catch (IOException E) {
       // handle
     }
