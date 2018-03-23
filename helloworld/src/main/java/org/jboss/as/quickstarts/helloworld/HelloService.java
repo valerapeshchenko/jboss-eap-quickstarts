7f5e570b6f2550501e7d7d3fb2dad80d0b8d148f
valerapeshchenko <35956219+valerapeshchenko@users.noreply.github.com>
2018-03-23 15:57:55 +0200

    Update HelloService.java

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -44,8 +44,8 @@ public class HelloService {
         mf.read(Thread.currentThread().getContextClassLoader().getResourceAsStream("META-INF/MANIFEST.MF"));
 
         Attributes atts = mf.getMainAttributes();        
-        return_string += "Implementation-Version: " + atts.getValue("Implementation-Version");
-        return_string += "Implementation-Build: " + atts.getValue("Implementation-Build");
+        return_string += "Implementation-Version: " + atts.getValue("Implementation-Version") + "\n";
+        return_string += "Implementation-Build: " + atts.getValue("Implementation-Build")+ "\n";
         return_string += "Specification-Vendor: " + atts.getValue("Specification-Vendor");
         } catch(Exception e) {      
     }        
